57fca466fd6fa2816d61e75e97e08c1ea65898c1
Amer411 <155028715+Amer411@users.noreply.github.com>
2026-06-04 07:36:45 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ============================
 // Service Worker - Offline Support
 // ============================
-const CACHE_NAME = 'almnhaj-v17';
+const CACHE_NAME = 'almnhaj-v18';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
